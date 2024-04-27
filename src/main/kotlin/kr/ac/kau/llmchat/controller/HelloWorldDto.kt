2ce07b93be3a33eb212a90914c0f575ac3d0796a
Youngmin Koo <youngminz.kr@gmail.com>
2024-04-27 18:30:28 +0900

    feat: Hello World API 제거

--- src/main/kotlin/kr/ac/kau/llmchat/controller/HelloWorldDto.kt
+++
@@ -1,8 +0,0 @@
-package kr.ac.kau.llmchat.controller
-
-sealed class HelloWorldDto {
-    data class Response(
-        val id: Long,
-        val name: String,
-    )
-}
--- src/main/kotlin/kr/ac/kau/llmchat/domain/HelloWorldRepository.kt
+++
@@ -1,5 +0,0 @@
-package kr.ac.kau.llmchat.domain
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface HelloWorldRepository : JpaRepository<HelloWorldEntity, Long>
