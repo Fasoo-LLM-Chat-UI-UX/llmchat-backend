08a6d509f118473a17e547a4727bb6a9e89ae2b7
Youngmin Koo <youngminz.kr@gmail.com>
2024-04-28 20:37:48 +0900

    feat: 보안 추가된 API에 Swagger 인증 추가

--- src/main/kotlin/kr/ac/kau/llmchat/configuration/SwaggerConfiguration.kt
+++ src/main/kotlin/kr/ac/kau/llmchat/configuration/SwaggerConfiguration.kt
@@ -4,10 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import io.swagger.v3.core.converter.ModelConverters
 import io.swagger.v3.core.jackson.ModelResolver
 import io.swagger.v3.core.jackson.TypeNameResolver
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import jakarta.annotation.PostConstruct
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@SecurityScheme(
+    name = "Authorization",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+)
 class SwaggerConfiguration(private val objectMapper: ObjectMapper) {
     @PostConstruct
     fun initialize() {
--- src/main/kotlin/kr/ac/kau/llmchat/controller/auth/AuthController.kt
+++ src/main/kotlin/kr/ac/kau/llmchat/controller/auth/AuthController.kt
@@ -1,9 +1,9 @@
 package kr.ac.kau.llmchat.controller.auth
 
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import kr.ac.kau.llmchat.domain.auth.UserEntity
 import kr.ac.kau.llmchat.service.auth.AuthService
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/auth")
 class AuthController(
     private val authService: AuthService,
-    @Value("\${llmchat.auth.jwt-secret}") secretKey: String,
 ) {
     @PostMapping("/register-by-username")
     @ApiResponse(responseCode = "201", description = "Created")
@@ -37,6 +36,7 @@ class AuthController(
     }
 
     @GetMapping("/ping")
+    @SecurityRequirement(name = "Authorization")
     fun ping(): ResponseEntity<String> {
         val user = SecurityContextHolder.getContext().authentication.principal as UserEntity
         return ResponseEntity.ok("Pong, ${user.username}!")
